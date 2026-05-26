79ad9b802ee7b3c2af56e986860a03d7de05118e
Aishi De <aishibde@gmail.com>
2026-05-26 06:54:48 +0530

    Added datatype.c filr

---
+++ datatype.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    //AISHI DE
+    printf("Basics\n\n");
+
+    int a = 10;
+    float f = 3.14;
+    char ch = 'A';
+    
+    printf("a = %d \n" , a);
+    printf("f = %f \n", f);
+    printf("ch = %c \n", ch);
+    
+    return 0;
+}
