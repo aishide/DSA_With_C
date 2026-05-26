bb0f8dbc4555a06880bc09a888e68c6a5f53a72a
Aishi De <aishibde@gmail.com>
2026-05-26 07:18:50 +0530

    Added file of printing number from 1 to 10 file

---
+++ print_from_1_to_10.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    //AISHI DE
+    printf("Number from 1 to 10\n\n");
+
+    //for (initialization; condition; update)
+    int i = 1;
+    for (i = 1; i<11; i++) { 
+        printf("%d\n", i);
+    }
+    return 0;
+}
