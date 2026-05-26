4e80bec219ae3d77bbcc35cc7768fc474a957b4f
Aishi De <aishibde@gmail.com>
2026-05-26 07:09:48 +0530

    Added greatest of two numbers file

---
+++ greatest_of_two_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    //AISHI DE
+    printf("Greatest of two numbers\n\n");
+
+    int a = 50;
+    int b = 460;
+
+    if (a > b) {
+        printf("the value of a that is %d is greater than value of b which is %d", a, b);
+    }
+    else {
+        printf("the value of b that is %d is greater than value of a which is %d", b, a);
+    }
+
+    return 0;
+}
