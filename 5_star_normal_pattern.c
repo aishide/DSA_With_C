0aab3cda7975e0c4bac574e5be1496472a4e1292
Aishi De <aishibde@gmail.com>
2026-05-27 05:49:27 +0530

    Added file of star pattern

---
+++ 5_star_normal_pattern.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+int main() {
+    
+    //AISHI DE
+    //*****
+    //*****
+    //*****
+    //*****
+    //*****
+    
+    for (int i = 1 ; i < 6 ; i ++ ) {
+        for (int j = 1; j < 6 ; j++) { 
+            printf("*\t");
+    }
+    printf("\n");
+  
+}
+return 0;
+}
