e3d5cb44f859ee514f89b5973bf4fb4924031632
Aishi De <aishibde@gmail.com>
2026-05-27 05:58:44 +0530

    Added file of doing star pattern in decresing format using c

---
+++ star_pattern_decresing.c
@@ -0,0 +1,61 @@
+
+#include <stdio.h> 
+int main() {
+    
+    //AISHI DE
+    
+/*
+
+****
+***
+**
+*
+
+*/
+    /*
+    for() //OUTER (rows)
+    {
+        for() //Space
+        {
+            decement
+        }
+        for() //STAR
+        {
+            increment
+        }
+    }
+    */
+    for (int i = 0 ; i <= 5 ; i ++ ) {
+        for (int j = 4 ; j > i ; j--){
+            printf(" * ");
+        }
+        for(int k = 0; k < i ; k++) { 
+            printf("  ");
+        }
+        printf("\n");
+    }
+    
+return 0;
+
+// inner loop condition is always checked with the row 
+}
+
+/*
+orr
+
+
+#include <stdio.h> 
+int main() {
+
+    //AISHI DE
+
+    for (int i = 0; i <5 ; i ++){
+        for (int j = 5 ; j > i; j--){
+            printf(" * ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+    
+*/
