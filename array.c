be949c806f03b7832f527874343eb92ea49cd5af
Aishi De <aishibde@gmail.com>
2026-05-27 05:51:20 +0530

    Added file of basics of array

---
+++ array.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main () {
+
+
+    //aishi de
+    
+    int arr[5];
+    printf("%d\n", arr[0]);
+    printf("%d\n", arr[1]);
+    printf("%d\n", arr[2]);
+    printf("%d\n", arr[3]);
+    printf("%d\n", arr[4]);
+
+printf("\n\n");
+
+    arr[0] = 12;
+    arr[1] = 34;
+    arr[2] = 76;
+    arr[3] = 43;
+    arr[4] = 87;
+
+
+    printf("%d\n", arr[0]);
+    printf("%d\n", arr[1]);
+    printf("%d\n", arr[2]);
+    printf("%d\n", arr[3]);
+    printf("%d\n", arr[4]);
+    
+return 0;
+
+// to access values of array we need loops
+}
