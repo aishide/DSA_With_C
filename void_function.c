e6f05fdeb971f9e4c5e55c7e694253284d10cbbf
Aishi De <aishibde@gmail.com>
2026-05-28 08:11:16 +0530

    Added the file of using a void function to greet in main function

---
+++ void_function.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void greet() {
+        printf("Hello!! I am Aishi!!\n");
+    }
+    
+int main() {
+    
+    //AISHI DE
+    //Function 
+    
+    /*
+    Syntax : 
+    return_type function_name(parameters) {
+    // code to execute
+    return value; // optional (depends on return_type)
+}
+    */
+    
+    //Simple function  
+    
+    greet();
+    
+    return 0;
+}
