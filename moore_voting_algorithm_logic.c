f5a5a745d5144f04af3e7b2822a600f2fd30bf4a
Aishi De <aishibde@gmail.com>
2026-05-30 07:49:18 +0530

    Added file of the logic of moore's voting algo in the ofrm of a example

---
+++ moore_voting_algorithm_logic.c
@@ -0,0 +1,30 @@
+//Moore's voting Algorithm 
+
+/*
+[2 2 1 1 1 2 2]
+
+majority = 2
+votes = 1
+
+majority = 2
+votes = 1/  2
+
+majority = 2
+votes = 1/  2/ 1
+
+majority = 2/ 1
+votes = 1/  2/  1/  0
+
+majority = 2/ 1
+votes = 1/  2/  1/  0/ 1 
+
+majority = 2/ 1/ 2 
+votes = 1/  2/  1/  0/  1/  0
+
+majority = 2/ 1/ 2 
+votes = 1/  2/  1/  0/  1/  0/  1
+
+
+MAJORITY ELEMENT = 2
+
+*/
